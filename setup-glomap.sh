1ce4b5874d2828b0d9c687f3647cdbc81c2c2bc6
Aleks <crni.aki@gmail.com>
2025-05-30 06:09:57 +0200

    updates

--- setup-glomap.sh
+++ setup-glomap.sh
@@ -38,7 +38,7 @@ mkdir glomap && cd glomap
 git clone https://github.com/colmap/glomap.git .
 mkdir build
 cd build
-cmake .. -GNinja
+cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=all
 ninja && ninja install
 
 glomap
