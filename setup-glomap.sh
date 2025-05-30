4a18b1628d155f33ab4299793e9385cd69df034a
Aleks <crni.aki@gmail.com>
2025-05-30 05:15:54 +0200

    updates

--- setup-glomap.sh
+++ setup-glomap.sh
@@ -24,6 +24,11 @@ apt-get install -y \
     nvidia-cuda-toolkit \
     nvidia-cuda-toolkit-gcc
 
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.tar.gz
+tar xfvz cmake-3.30.1.tar.gz && cd cmake-3.30.1
+./bootstrap && make -j$(nproc) && sudo make install
+
 apt autoremove -y
 
 cd /opt
