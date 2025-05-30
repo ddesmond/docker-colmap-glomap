1a5dcc01863990d2405f4411a3d94308793e130d
Aleks <crni.aki@gmail.com>
2025-05-30 05:42:17 +0200

    updates

--- setup-glomap.sh
+++ setup-glomap.sh
@@ -29,7 +29,7 @@ apt remove cmake -y
 cd /tmp
 wget https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.tar.gz
 tar xfvz cmake-3.30.1.tar.gz && cd cmake-3.30.1
-./bootstrap && make -j$(nproc) && sudo make install
+./bootstrap && make -j$(nproc) && make install
 
 apt autoremove -y
 
