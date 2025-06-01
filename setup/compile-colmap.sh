8b32212332b93ab461d1bb2ff7b816645695d19c
Aleks <crni.aki@gmail.com>
2025-06-01 13:50:06 +0200

    updates

--- setup/compile-colmap.sh
+++ setup/compile-colmap.sh
@@ -4,10 +4,16 @@ ln -s /usr/bin/ninja /usr/sbin/ninja
 
 find / -name "libcudss.so*"
 
+echo "Add cudss"
+cd /tmp && mkdir -p cudss && cd cudss
+wget https://developer.download.nvidia.com/compute/cudss/redist/libcudss/linux-x86_64/libcudss-linux-x86_64-0.3.0.9_cuda12-archive.tar.xz
+tar -xf libcudss-linux-x86_64-0.3.0.9_cuda12-archive.tar.xz
+export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
+export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:$LD_LIBRARY_PATH
 echo "Building Ceres"
 cd /tmp
 
-git clone https://github.com/ceres-solver/ceres-solver.git
+git clone --recurse-submodules https://github.com/ceres-solver/ceres-solver
 cd ceres-solver
 git pull
 git submodule update --init --recursive
