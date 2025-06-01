63dea95d5b8d57e7aedcbd2db09e16c6f84c202c
Aleks <crni.aki@gmail.com>
2025-06-01 19:15:35 +0200

    updates

--- setup/compile-colmap.sh
+++ setup/compile-colmap.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 ln -s /usr/bin/ninja /usr/sbin/ninja
-
-#ln -s /usr/local/lib/libboost_program_options.so.1.82.0 /usr/lib64/libboost_program_options.so.1.82.0
-#ln -s /usr/local/lib/libboost_graph.so.1.82.0 /usr/lib64/libboost_graph.so.1.82.0
 export PATH=/usr/local/lib:$PATH
-# extra deps
 export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
 export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:/usr/local/lib:$LD_LIBRARY_PATH
 
 
 # COLMAP BUILD
 echo "Building COLMAP with GPU support"
+mkdir /colmap-install
+
 cd /opt
 mkdir colmap && cd colmap
 git clone https://github.com/colmap/colmap.git .
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=80
+cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=80 -DCMAKE_INSTALL_PREFIX=/colmap-install
 ninja && ninja install
 
 colmap -h
--- setup/compile-glomap.sh
+++ setup/compile-glomap.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+ln -s /usr/bin/ninja /usr/sbin/ninja
+export PATH=/usr/local/lib:$PATH
+export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
+export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:/usr/local/lib:$LD_LIBRARY_PATH
+
+
+
 # GLOMAP BUILD
+mkdir /glomap-install
 cd /opt
 mkdir glomap && cd glomap
 git clone https://github.com/colmap/glomap.git .
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=all
+cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=all -DCMAKE_INSTALL_PREFIX=/glomap-install
 ninja && ninja install
 
 glomap -v
