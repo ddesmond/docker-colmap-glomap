e33b9b7495bfa060d0a6867189e5475f88e62c05
Aleks <crni.aki@gmail.com>
2025-06-01 19:06:28 +0200

    updates

--- setup/compile-colmap.sh
+++ setup/compile-colmap.sh
@@ -2,9 +2,9 @@
 
 ln -s /usr/bin/ninja /usr/sbin/ninja
 
-ln -s /usr/local/lib/libboost_program_options.so.1.82.0 /usr/lib64/libboost_program_options.so.1.82.0
-ln -s /usr/local/lib/libboost_graph.so.1.82.0 /usr/lib64/libboost_graph.so.1.82.0
-
+#ln -s /usr/local/lib/libboost_program_options.so.1.82.0 /usr/lib64/libboost_program_options.so.1.82.0
+#ln -s /usr/local/lib/libboost_graph.so.1.82.0 /usr/lib64/libboost_graph.so.1.82.0
+cp -f /usr/local/lib/libboost_* /usr/lib64/
 # extra deps
 export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
 export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:$LD_LIBRARY_PATH
