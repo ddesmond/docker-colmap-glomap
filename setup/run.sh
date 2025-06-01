c3b6d76cbe4847ef9b38b48ecb3a3ce31321bcdc
Aleks <crni.aki@gmail.com>
2025-06-01 22:14:07 +0200

    updates

--- setup/run.sh
+++ setup/run.sh
@@ -3,8 +3,8 @@ echo "Running colmap/glomap"
 
 export PATH=/usr/local/lib:$PATH
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-updatedb
-locate libcudss
+export cudss_DIR=/opt/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
+export LD_LIBRARY_PATH=/opt/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:$LD_LIBRARY_PATH
 
 colmap -h
 glomap -h
