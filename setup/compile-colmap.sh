0675cb73daa7432cdeede06f54ffd9493df4bf5c
Aleks <crni.aki@gmail.com>
2025-06-01 15:02:54 +0200

    updates

--- setup/compile-colmap.sh
+++ setup/compile-colmap.sh
@@ -27,6 +27,17 @@ mkdir build && cd build
 cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_CUDA_ARCHITECTURES=80
 make -j$(nproc) && make install
 
+# build cgal
+echo "Building CGAL"
+cd /tmp && mkdir cgal && cd cgal
+wget https://github.com/CGAL/cgal/archive/refs/tags/v5.6.2.tar.gz
+tar xvfz v5.6.2.tar.gz
+mv v5.6.2 cgal_v5.6.2
+cd cgal_v5.6.2
+mkdir build
+cd build
+cmake ..
+make install
 
 
 # COLMAP BUILD
