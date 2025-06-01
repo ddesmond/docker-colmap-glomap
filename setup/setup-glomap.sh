aa894cc744c95b0471dc6549103780d958c1a3b9
Aleks <crni.aki@gmail.com>
2025-06-01 08:40:06 +0200

    updates

--- setup/setup-glomap.sh
+++ setup/setup-glomap.sh
@@ -27,18 +27,3 @@ dnf install -y \
     nvidia-cuda-toolkit-gcc
 
 dnf remove cmake -y
-
-cd /tmp
-wget https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.tar.gz
-tar xfvz cmake-3.30.1.tar.gz && cd cmake-3.30.1
-./bootstrap && make -j$(nproc) && make install
-
-cd /opt
-mkdir glomap && cd glomap
-git clone https://github.com/colmap/glomap.git .
-mkdir build
-cd build
-cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=all
-ninja && ninja install
-
-glomap
