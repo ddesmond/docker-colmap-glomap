0dbb6c23dbb4ca5e679ad7019baf4c5502632af7
Aleks <crni.aki@gmail.com>
2025-06-01 20:01:14 +0200

    updates

--- setup/compile-colmap.sh
+++ setup/compile-colmap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s /usr/bin/ninja /usr/sbin/ninja
+
 export PATH=/usr/local/lib:$PATH
 export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
 export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:/usr/local/lib:$LD_LIBRARY_PATH
@@ -18,6 +18,6 @@ cd build
 cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=80 -DCMAKE_INSTALL_PREFIX=/colmap-install
 ninja && ninja install
 
-colmap -h
+/colmap-install/bin/colmap -h
 
 ls -la /colmap-install
--- setup/compile-glomap.sh
+++ setup/compile-glomap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s /usr/bin/ninja /usr/sbin/ninja
+
 export PATH=/usr/local/lib:$PATH
 export cudss_DIR=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/cmake/cudss
 export LD_LIBRARY_PATH=/tmp/cudss/libcudss-linux-x86_64-0.3.0.9_cuda12-archive/lib/:/usr/local/lib:$LD_LIBRARY_PATH
